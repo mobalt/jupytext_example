b2dbf832ffa0d6efc229f8f7a79490c397f91532
Moises Baltazar <me@moisesb.com>
2020-02-20 15:10:23 -0600

    Try "light" format instead of "percentage"

--- notebook.py
+++ notebook.py
@@ -1,11 +1,11 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:percent
+#     formats: ipynb,py:light
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
+#       format_name: light
+#       format_version: '1.5'
 #       jupytext_version: 1.3.3
 #   kernelspec:
 #     display_name: Python 3
@@ -13,36 +13,28 @@
 #     name: python3
 # ---
 
-# %%
 import pandas as pd
 
-# %%
 patients = pd.read_csv('patient-accounts.csv')
 patients.head()
 
-# %% [markdown]
 # # Does drug x cause an age-dependent increase in patient paranoia?
 #
 # To answer this question we use a marker of paranoia, password length.
 
-# %%
 patients['paranoia'] = patients.password.str.len()
 patients.paranoia.hist()
 
-# %%
 patients.age.hist()
 
-# %%
 patients.plot.scatter('age','paranoia')
 
-# %%
 patients.corr()
 
-# %% [markdown]
 # # Results
 #
 # Nope, sorry.
 #
 # ![](https://media.ldscdn.org/images/media-library/book-of-mormon-seminary-curriculum-images/boy-sad-840560-mobile.jpg)
 
-# %%
+
